2681a64fcc9375aef940cdc9b63433711186c4e6
HyeinKim <hyen.k.35@gmail.com>
2024-11-25 22:21:30 +0900

    chart, playlist 작업 완료

--- src/main/java/kr/heartbeat/controller/HomeController.java
+++ src/main/java/kr/heartbeat/controller/HomeController.java
@@ -16,11 +16,11 @@ public class HomeController {
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public String join() { return "heartbeat/join"; }  
 
-//	@RequestMapping(value="/chart", method = RequestMethod.GET)
-//	public String chart() { return "heartbeat/chart"; } 
+	/*@RequestMapping(value="/chart", method = RequestMethod.GET)
+	public String chart() { return "heartbeat/chart"; } */
 	
-	@RequestMapping(value="/playlist", method = RequestMethod.GET)
-	public String playlist() { return "heartbeat/playlist"; } 
+	/*@RequestMapping(value="/playlist", method = RequestMethod.GET)
+	public String playlist() { return "heartbeat/playlist"; } */
 	
 	@RequestMapping(value="/community", method = RequestMethod.GET)
 	public String community() { return "community/community"; } 
